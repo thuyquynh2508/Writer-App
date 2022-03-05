b8e64f2e2334b138ca64ed92c781ffd9fcec960c
thuyquynh2508 <thuyquynh404@gmail.com>
2022-03-05 21:05:38 +0700

    commit

--- script/function.js
+++ script/function.js
@@ -1,36 +1,42 @@
-const filePage = document.querySelector('.file-page');
-const recentPage = document.querySelector('.recent-page');
+const filePage = document.querySelector(".file-page");
+const recentPage = document.querySelector(".recent-page");
 
-const fileContent = document.getElementById('text');
-const fileList = document.querySelector('.file-recent');
-const inputName = document.querySelector('.file-info__name-text');
+const fileContent = document.getElementById("text");
+const fileList = document.querySelector(".file-recent");
+const inputName = document.querySelector(".file-info__name-text");
 
-const openBtn = document.querySelector('.file-option__open');
-const returnBtn = document.querySelector('.recent-nav__return');
-const saveBtn = document.querySelector('.file-option__save');
-const newBtn = document.querySelector('.file-option__new');
-const renameBtn = document.querySelector('.file-option__rename');
-const deleteBtn = document.querySelector('.file-option__delete');
+const fileOption = document.querySelector(".file-bar__file-option");
+const openBtn = document.querySelector(".file-option__open");
+const returnBtn = document.querySelector(".recent-nav__return");
+const saveBtn = document.querySelector(".file-option__save");
+const newBtn = document.querySelector(".file-option__new");
+const renameBtn = document.querySelector(".file-option__rename");
+const deleteBtn = document.querySelector(".file-option__delete");
+const detailBtn = document.querySelector(".file-option__detail");
 
+const detailModal = document.querySelector(".detail-modal-container");
+const detailBody = document.querySelector(".detail-modal__body");
+const closeModal = document.querySelector(".detail-modal__close");
+const modalContainer = document.querySelector(".detail-modal-container");
 showNewFile();
 showRecentList();
 function showRecentPage() {
-  filePage.classList.add('remove');
-  recentPage.classList.add('open');
+  filePage.classList.add("remove");
+  recentPage.classList.add("open");
 }
 
 function showFilePage() {
-  recentPage.classList.remove('open');
-  filePage.classList.remove('remove');
+  recentPage.classList.remove("open");
+  filePage.classList.remove("remove");
 }
-openBtn.addEventListener('click', showRecentPage);
-returnBtn.addEventListener('click', showFilePage);
+openBtn.addEventListener("click", showRecentPage);
+returnBtn.addEventListener("click", showFilePage);
 
-newBtn.addEventListener('click', function(e) {
+newBtn.addEventListener("click", function (e) {
   e.preventDefault();
   showNewFile();
   showRecentList();
-})
+});
 function getFileList() {
   let getFileLocalStorage = localStorage.getItem("New file");
   if (getFileLocalStorage == null) {
@@ -40,10 +46,10 @@ function getFileList() {
   }
   return fileArr;
 }
-saveBtn.addEventListener('click', function(e) {
+saveBtn.addEventListener("click", function (e) {
   e.preventDefault();
   let fileArr = getFileList();
-  let openIndex = this.getAttribute("indexOpen");
+  let openIndex = this.getAttribute("indexSave");
   let fileIndex;
   if (openIndex == 0 || openIndex) {
     fileIndex = openIndex;
@@ -53,11 +59,10 @@ saveBtn.addEventListener('click', function(e) {
   fileArr[fileIndex].name = inputName.value;
   fileArr[fileIndex].content = CKEDITOR.instances.text.getData();
   fileArr[fileIndex].timeLastModified = TimeStr();
-  this.removeAttribute("indexOpen");
-  localStorage.setItem("New file", JSON.stringify(fileArr)); 
-})
+  localStorage.setItem("New file", JSON.stringify(fileArr));
+});
 
-renameBtn.addEventListener('click', function() {
+renameBtn.addEventListener("click", function () {
   let fileArr = getFileList();
   let renameIndex = this.getAttribute("indexRename");
   let fileIndex;
@@ -66,58 +71,61 @@ renameBtn.addEventListener('click', function() {
   } else {
     fileIndex = fileArr.length - 1;
   }
-  document.getElementById('name-input').focus();
-  document.getElementById('name-input').select();
-  inputName.addEventListener('keypress', (e) => {
-    if (e.key == 'Enter') {
+  document.getElementById("name-input").focus();
+  document.getElementById("name-input").select();
+  inputName.addEventListener("keypress", (e) => {
+    if (e.key == "Enter") {
       fileArr[fileIndex].name = inputName.value;
-      localStorage.setItem("New file", JSON.stringify(fileArr)); 
+      localStorage.setItem("New file", JSON.stringify(fileArr));
       showRecentList();
     }
-  })
-  this.removeAttribute("indexRename");
+  });
 });
 
-document.getElementById('name-input').addEventListener('focus', function() {
-
-})
+document.getElementById("name-input").addEventListener("focus", function () {});
 function createNewFile() {
   let fileArr = getFileList();
-  let fileName = 'Untitled document';
-  let fileText = '';
+  let fileName = "Untitled document";
+  let fileText = "";
   let fileExist = true;
   let fileTimeCreate = TimeStr();
   let fileTimeLastModified = TimeStr();
-  fileArr.push ({
+  fileArr.push({
     name: fileName,
     content: fileText,
     isExist: fileExist,
     timeCreate: fileTimeCreate,
-    timeLastModified: fileTimeLastModified
+    timeLastModified: fileTimeLastModified,
   });
   localStorage.setItem("New file", JSON.stringify(fileArr));
 }
 function showNewFile() {
   createNewFile();
-  inputName.value = 'Untitled document';
+  inputName.value = "Untitled document";
   CKEDITOR.instances.text.setData("");
 }
 
 // Xây dựng đối tượng Time
-function TimeFormat(day,month,year,hour,minute) {
+function TimeFormat(day, month, year, hour, minute) {
   this.day = day;
   this.month = month;
   this.year = year;
   this.hour = hour;
   this.minute = minute;
-  this.getTime = function() {
-    return `${this.day}/${this.month}/${this.year} ${hour}:${minute}` 
-  }
+  this.getTime = function () {
+    return `${this.day}/${this.month}/${this.year} ${hour}:${minute}`;
+  };
 }
 
 function TimeStr() {
   let now = new Date();
-  let nowObj = new TimeFormat(now.getDate(),now.getMonth()+1, now.getFullYear(), now.getHours(), now.getMinutes());
+  let nowObj = new TimeFormat(
+    now.getDate(),
+    now.getMonth() + 1,
+    now.getFullYear(),
+    now.getHours(),
+    now.getMinutes()
+  );
   return nowObj.getTime();
 }
 
@@ -126,41 +134,117 @@ function showRecentList() {
   let newFileItem = "";
   fileArr.forEach((element, index) => {
     newFileItem += `<li class="file-recent__info" onclick="openFile(${index})">
-    <div class="file-recent-wrap">
-      <div class="file-recent-icon">
-        <i class="far fa-file-alt"></i>
+      <div class="file-recent-wrap">
+        <div class="file-recent-icon">
+          <i class="far fa-file-alt"></i>
+        </div>
+        <div class="file-recent-name">
+          <h3 class="file-recent-name__name">${fileArr[index].name}</h3>
+          <p class="file-recent-name__location">Web</p>
+        </div>
       </div>
-      <div class="file-recent-name">
-        <h3 class="file-recent-name__name">${fileArr[index].name}</h3>
-        <p class="file-recent-name__location">Web</p>
+      <div class="file-recent-date">
+        ${fileArr[index].timeCreate}
       </div>
-    </div>
-    <div class="file-recent-date">
-      ${fileArr[index].timeCreate}
-    </div>
-  </li>`
+    </li>`;
   });
   fileList.innerHTML = newFileItem;
 }
 
 function openFile(index) {
   let fileArr = getFileList();
-  saveBtn.setAttribute("indexOpen", index);
+  saveBtn.setAttribute("indexSave", index);
   renameBtn.setAttribute("indexRename", index);
   deleteBtn.setAttribute("indexDelete", index);
+  detailBtn.setAttribute("indexDetail", index);
   inputName.value = fileArr[index].name;
-  CKEDITOR.instances['text'].setData(fileArr[index].content);
+  CKEDITOR.instances["text"].setData(fileArr[index].content);
   fileArr[index].timeLastModified = TimeStr();
   showFilePage();
 }
 
-
-deleteBtn.addEventListener('click', function(e) {
+deleteBtn.addEventListener("click", function (e) {
   e.preventDefault();
   let fileArr = getFileList();
   let deleteIndex = this.getAttribute("indexDelete");
   fileArr.splice(deleteIndex, 1);
   localStorage.setItem("New file", JSON.stringify(fileArr));
+  this.removeAttribute("indexDelete");
   showRecentList();
   showRecentPage();
-})
+});
+
+detailBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  let fileArr = getFileList();
+  let detailIndex = this.getAttribute("indexDetail");
+  let fileIndex;
+  if (detailIndex == 0 || detailIndex) {
+    fileIndex = detailIndex;
+  } else {
+    fileIndex = fileArr.length - 1;
+  }
+  let detailFile = `<div class="detail-modal__section">
+  <h3 class="detail-modal__section-header">Properties</h3>
+  <ul class="detail-modal__section-body">
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Name</p>
+      <p class="detail-modal__item-content">${fileArr[fileIndex].name}</p>
+    </li>
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Comment</p>
+      <p class="detail-modal__item-content">Javascript</p>
+    </li>
+  </ul>
+</div>
+<div class="detail-modal__section">
+  <h3 class="detail-modal__section-header">Related Dates</h3>
+  <ul class="detail-modal__section-body">
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Last Modified</p>
+      <p class="detail-modal__item-content">${fileArr[fileIndex].timeLastModified}</p>
+    </li>
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Created</p>
+      <p class="detail-modal__item-content">${fileArr[fileIndex].timeCreate}</p>
+    </li>
+  </ul>
+</div>
+<div class="detail-modal__section">
+  <h3 class="detail-modal__section-header">Related People</h3>
+  <ul class="detail-modal__section-body">
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Author</p>
+      <p class="detail-modal__item-content">
+        Quynh Nguyen</p>
+    </li>
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Last Modified By</p>
+      <p class="detail-modal__item-content">
+        Quynh Nguyen</p>
+    </li>
+  </ul>
+</div>
+<div class="detail-modal__section">
+  <h3 class="detail-modal__section-header">Related Document</h3>
+  <ul class="detail-modal__section-body">
+    <li class="detail-modal__item">
+      <p class="detail-modal__item-label">Location</p>
+      <p class="detail-modal__item-content">
+        <i class="fas fa-folder-open"></i>
+        Web
+      </p>
+    </li>
+  </ul>
+</div>`;
+  detailBody.innerHTML = detailFile;
+  detailModal.classList.add("open");
+});
+
+function hideDetailModal() {
+  detailModal.classList.remove("open");
+}
+closeModal.addEventListener("click", hideDetailModal);
+modalContainer.addEventListener("click", function (event) {
+  event.stopPropagation();
+});
